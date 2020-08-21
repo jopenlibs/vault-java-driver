4721d8e49eb59d1e6a680d1061e90edf38f4ba5e
sean.dalby <sean.dalby@taulia.com>
2020-08-21 17:50:08 -0600

    Add options to write and metadata to read

--- src/main/java/io/github/jopenlibs/vault/api/Logical.java
+++ src/main/java/io/github/jopenlibs/vault/api/Logical.java
@@ -29,6 +29,8 @@ import static io.github.jopenlibs.vault.api.LogicalUtilities.jsonObjectToWriteFr
  */
 public class Logical extends OperationsBase {
 
+    private static final WriteOptions EMPTY_WRITE_OPTIONS = new WriteOptions().build();
+
     private String nameSpace;
 
     public enum logicalOperations {authentication, deleteV1, deleteV2, destroy, listV1, listV2, readV1, readV2, writeV1, writeV2, unDelete, mount}
@@ -201,9 +203,9 @@ public class Logical extends OperationsBase {
     public LogicalResponse write(final String path, final Map<String, Object> nameValuePairs)
             throws VaultException {
         if (engineVersionForSecretPath(path).equals(2)) {
-            return write(path, nameValuePairs, logicalOperations.writeV2, null);
+            return write(path, nameValuePairs, logicalOperations.writeV2, null, EMPTY_WRITE_OPTIONS);
         } else {
-            return write(path, nameValuePairs, logicalOperations.writeV1, null);
+            return write(path, nameValuePairs, logicalOperations.writeV1, null, EMPTY_WRITE_OPTIONS);
         }
     }
 
@@ -238,47 +240,35 @@ public class Logical extends OperationsBase {
             final Integer wrapTTL)
             throws VaultException {
         if (engineVersionForSecretPath(path).equals(2)) {
-            return write(path, nameValuePairs, logicalOperations.writeV2, wrapTTL);
+            return write(path, nameValuePairs, logicalOperations.writeV2, wrapTTL, EMPTY_WRITE_OPTIONS);
         } else {
-            return write(path, nameValuePairs, logicalOperations.writeV1, wrapTTL);
+            return write(path, nameValuePairs, logicalOperations.writeV1, wrapTTL, EMPTY_WRITE_OPTION);
         }
     }
 
+    public LogicalResponse write(final String path, final Map<String, Object> nameValuePairs,
+            final WriteOptions writeOptions)
+            throws VaultException {
+        if (this.engineVersionForSecretPath(path) != 2) {
+            throw new VaultException("Write options are only supported in KV Engine version 2.");
+        }
+	// TODO handle wrapTTL
+        return write(path, nameValuePairs, logicalOperations.writeV2, null, writeOptions);
+    }
+
     private LogicalResponse write(final String path, final Map<String, Object> nameValuePairs,
-            final logicalOperations operation, final Integer wrapTTL) throws VaultException {
+             final logicalOperations operation, final Integer wrapTTL, final WriteOptions writeOptions)
+             throws VaultException {
 
         return retry(attempt -> {
-            JsonObject requestJson = Json.object();
-            if (nameValuePairs != null) {
-                for (final Map.Entry<String, Object> pair : nameValuePairs.entrySet()) {
-                    final Object value = pair.getValue();
-                    if (value == null) {
-                        requestJson = requestJson.add(pair.getKey(), (String) null);
-                    } else if (value instanceof Boolean) {
-                        requestJson = requestJson.add(pair.getKey(), (Boolean) pair.getValue());
-                    } else if (value instanceof Integer) {
-                        requestJson = requestJson.add(pair.getKey(), (Integer) pair.getValue());
-                    } else if (value instanceof Long) {
-                        requestJson = requestJson.add(pair.getKey(), (Long) pair.getValue());
-                    } else if (value instanceof Float) {
-                        requestJson = requestJson.add(pair.getKey(), (Float) pair.getValue());
-                    } else if (value instanceof Double) {
-                        requestJson = requestJson.add(pair.getKey(), (Double) pair.getValue());
-                    } else if (value instanceof JsonValue) {
-                        requestJson = requestJson.add(pair.getKey(),
-                                (JsonValue) pair.getValue());
-                    } else {
-                        requestJson = requestJson.add(pair.getKey(),
-                                pair.getValue().toString());
-                    }
-                }
-            }
-            // Make an HTTP request to Vault
+            JsonObject dataJson = buildJsonFromMap(nameValuePairs);
+            JsonObject optionsJson = writeOptions.isEmpty() ? null : buildJsonFromMap(writeOptions.getOptionsMap());	
+             // Make an HTTP request to Vault
             final RestResponse restResponse = getRest()//NOPMD
                     .url(config.getAddress() + "/v1/" + adjustPathForReadOrWrite(path,
                             config.getPrefixPathDepth(), operation))
-                    .body(jsonObjectToWriteFromEngineVersion(operation, requestJson).toString()
-                            .getBytes(StandardCharsets.UTF_8))
+                    .body(jsonObjectToWriteFromEngineVersion(operation, dataJson, optionsJson).toString()
+                            .getBytes(StandardCharsets.UTF_8))		
                     .header("X-Vault-Token", config.getToken())
                     .header("X-Vault-Namespace", this.nameSpace)
                     .header("X-Vault-Request", "true")
@@ -607,4 +597,34 @@ public class Logical extends OperationsBase {
     public Integer getEngineVersionForSecretPath(final String path) {
         return this.engineVersionForSecretPath(path);
     }
+
+    private JsonObject buildJsonFromMap(Map<String, Object> nameValuePairs) {
+        JsonObject requestJson = Json.object();
+        if (nameValuePairs != null) {
+            for (final Map.Entry<String, Object> pair : nameValuePairs.entrySet()) {
+                final Object value = pair.getValue();
+                if (value == null) {
+                    requestJson = requestJson.add(pair.getKey(), (String) null);
+                } else if (value instanceof Boolean) {
+                    requestJson = requestJson.add(pair.getKey(), (Boolean) pair.getValue());
+                } else if (value instanceof Integer) {
+                    requestJson = requestJson.add(pair.getKey(), (Integer) pair.getValue());
+                } else if (value instanceof Long) {
+                    requestJson = requestJson.add(pair.getKey(), (Long) pair.getValue());
+                } else if (value instanceof Float) {
+                    requestJson = requestJson.add(pair.getKey(), (Float) pair.getValue());
+                } else if (value instanceof Double) {
+                    requestJson = requestJson.add(pair.getKey(), (Double) pair.getValue());
+                } else if (value instanceof JsonValue) {
+                    requestJson = requestJson.add(pair.getKey(),
+                            (JsonValue) pair.getValue());
+                } else {
+                    requestJson = requestJson.add(pair.getKey(),
+                            pair.getValue().toString());
+                }
+            }
+        }
+        return jsonObject;
+    }
+
 }
--- src/main/java/io/github/jopenlibs/vault/response/LogicalResponse.java
+++ src/main/java/io/github/jopenlibs/vault/response/LogicalResponse.java
@@ -25,6 +25,7 @@ public class LogicalResponse extends VaultResponse {
     private WrapResponse wrapResponse;
     private Boolean renewable;
     private Long leaseDuration;
+    private final Map<String, String> metadata = new HashMap<>();
 
     /**
      * @param restResponse The raw HTTP response from Vault.
@@ -62,8 +63,13 @@ public class LogicalResponse extends VaultResponse {
         return leaseDuration;
     }
 
+
     public WrapResponse getWrapResponse() {
         return wrapResponse;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
     }
 
     private void parseMetadataFields() {
@@ -91,16 +97,14 @@ public class LogicalResponse extends VaultResponse {
             }
             data = new HashMap<>();
             dataObject = jsonObject.get("data").asObject();
-            for (final JsonObject.Member member : dataObject) {
-                final JsonValue jsonValue = member.getValue();
-                if (jsonValue == null || jsonValue.isNull()) {
-                    continue;
-                } else if (jsonValue.isString()) {
-                    data.put(member.getName(), jsonValue.asString());
-                } else {
-                    data.put(member.getName(), jsonValue.toString());
-                }
+            parseJsonIntoMap(dataObject, data);
+
+            JsonValue metadataValue = jsonObject.get("metadata");
+            if (null != metadataValue) {
+                JsonObject metadataObject = metadataValue.asObject();
+                parseJsonIntoMap(metadataObject, metadata);
             }
+
             // For list operations convert the array of keys to a list of values
             if (operation.equals(Logical.logicalOperations.listV1) || operation.equals(
                     Logical.logicalOperations.listV2)) {
@@ -119,4 +123,18 @@ public class LogicalResponse extends VaultResponse {
         } catch (Exception ignored) {
         }
     }
+
+    private void parseJsonIntoMap(JsonObject jsonObject, Map<String, String> map) {
+        for (final JsonObject.Member member : jsonObject) {
+            final JsonValue jsonValue = member.getValue();
+            if (jsonValue == null || jsonValue.isNull()) {
+                continue;
+            } else if (jsonValue.isString()) {
+                map.put(member.getName(), jsonValue.asString());
+            } else {
+                map.put(member.getName(), jsonValue.toString());
+            }
+        }
+    }
+
 }
