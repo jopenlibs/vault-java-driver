9b2c5bc84fcde99ec0a64f63fa4eacf311c1be00
tledkov <tledkov@apache.org>
2022-10-14 12:18:28 +0300

    VJD-5 fix import

--- src/test/java/io/github/jopenlibs/vault/vault/api/AuthUnwrapTest.java
+++ src/test/java/io/github/jopenlibs/vault/vault/api/AuthUnwrapTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 
 public class AuthUnwrapTest {
 
