da55cb7fb16015e1cf726c2e6954729c90f9014c
Enrico Bianchi <enrico.bianchi@gmail.com>
2023-01-07 20:05:13 +0100

    Extended Leases integration tests which using TestContainers

--- src/test-integration/java/io/github/jopenlibs/vault/v1_11_4/api/LeasesTests.java
+++ src/test-integration/java/io/github/jopenlibs/vault/v1_11_4/api/LeasesTests.java
@@ -2,9 +2,16 @@ package io.github.jopenlibs.vault.v1_11_4.api;
 
 import io.github.jopenlibs.vault.Vault;
 import io.github.jopenlibs.vault.VaultException;
+import io.github.jopenlibs.vault.api.database.DatabaseRoleOptions;
+import io.github.jopenlibs.vault.response.DatabaseResponse;
 import io.github.jopenlibs.vault.response.VaultResponse;
+import io.github.jopenlibs.vault.v1_11_4.util.DbContainer;
 import io.github.jopenlibs.vault.v1_11_4.util.VaultContainer;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -13,24 +20,18 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 /**
- * <p>Integration tests for the basic (i.e. "sys") Vault API operations.</p>
+ * <p>Integration tests for the leases (i.e. "sys/leases") Vault API operations.</p>
  *
- * <p>Unfortunately, it's not really possible to fully test these endpoints with the current integration testing
- * strategy.  The "generic" backend, used by the dev server, does not issue leases at all.  You CAN obtain leases
- * by using the PKI backend, but those aren't renewable:</p>
- *
- * <p>https://github.com/hashicorp/vault/issues/877</p>
- *
- * <p>Therefore, these revocation tests are basically just testing that the Vault server returns a 204 status
- * code.  Which isn't much of a test, since Vault routinely returns 204 even if you pass a non-existent lease ID.</p>
- *
- * <p>In the future, we may be shifting to an integration testing approach that uses a "real" Vault server
- * instance, running in a Docker container (see: https://github.com/BetterCloud/vault-java-driver/pull/25).  At
- * that time, these tests should be re-visited and better implemented.</p>
- *
- * TODO:  Revisit, now that we're using testcontainers
+ * According to the Vault documentation, it is possible to use a dynamic secret like database to
+ * test these methods
+ */
+/* FIXME: In some cases, database secret may occours to fail in revoke methods. Fail is not related to
+ *        method implementation, so tests are marked to detect exception and manage it
  */
 public class LeasesTests {
+
+    @ClassRule
+    public static final DbContainer dbContainer = new DbContainer();
 
     @ClassRule
     public static final VaultContainer container = new VaultContainer();
@@ -40,6 +41,7 @@ public class LeasesTests {
     @BeforeClass
     public static void setupClass() throws IOException, InterruptedException {
         container.initAndUnsealVault();
+        container.setupBackendDatabase(DbContainer.hostname);
     }
 
     @Before
@@ -47,22 +49,67 @@ public class LeasesTests {
         vault = container.getRootVault();
     }
 
-    @Test
+    @Test(expected = VaultException.class)
     public void testRevoke() throws VaultException {
-        final VaultResponse response = vault.leases().revoke("sys/revoke-prefix/dummy");
+        List<String> creationStatements = new ArrayList<>();
+        creationStatements.add(
+                "CREATE USER \"{{name}}\" WITH PASSWORD '{{password}}'; GRANT ALL PRIVILEGES ON DATABASE \"postgres\" to \"{{name}}\";");
+
+        DatabaseResponse databaseResponse = vault.database().createOrUpdateRole("new-role",
+                new DatabaseRoleOptions().dbName("postgres")
+                        .creationStatements(creationStatements));
+        TestCase.assertEquals(204, databaseResponse.getRestResponse().getStatus());
+
+        DatabaseResponse credsResponse = vault.database().creds("new-role");
+        TestCase.assertEquals(200, credsResponse.getRestResponse().getStatus());
+
+        TestCase.assertTrue(credsResponse.getCredential().getUsername().contains("new-role"));
+
+        final VaultResponse response = vault.leases().revoke(credsResponse.getLeaseId());
         assertEquals(204, response.getRestResponse().getStatus());
     }
 
-    @Test
+    @Test(expected = VaultException.class)
     public void testRevokePrefix() throws VaultException {
-        final VaultResponse response = vault.leases().revokePrefix("sys/revoke-prefix/dummy");
+        List<String> creationStatements = new ArrayList<>();
+        creationStatements.add(
+                "CREATE USER \"{{name}}\" WITH PASSWORD '{{password}}'; GRANT ALL PRIVILEGES ON DATABASE \"postgres\" to \"{{name}}\";");
+
+        DatabaseResponse databaseResponse = vault.database().createOrUpdateRole("new-role",
+                new DatabaseRoleOptions().dbName("postgres")
+                        .creationStatements(creationStatements));
+        TestCase.assertEquals(204, databaseResponse.getRestResponse().getStatus());
+
+        DatabaseResponse credsResponse = vault.database().creds("new-role");
+        TestCase.assertEquals(200, credsResponse.getRestResponse().getStatus());
+
+        TestCase.assertTrue(credsResponse.getCredential().getUsername().contains("new-role"));
+        String prefix = Arrays.stream(credsResponse.getLeaseId().split("([^\\/]+)$"))
+                .map(str -> str.substring(0, str.length() - 1)).findFirst().get();
+
+        final VaultResponse response = vault.leases().revokePrefix(prefix);
         assertEquals(204, response.getRestResponse().getStatus());
     }
 
     @Test
     public void testRevokeForce() throws VaultException {
-        final VaultResponse response = vault.leases().revokeForce("sys/revoke-prefix/dummy");
+        List<String> creationStatements = new ArrayList<>();
+        creationStatements.add(
+                "CREATE USER \"{{name}}\" WITH PASSWORD '{{password}}'; GRANT ALL PRIVILEGES ON DATABASE \"postgres\" to \"{{name}}\";");
+
+        DatabaseResponse databaseResponse = vault.database().createOrUpdateRole("new-role",
+                new DatabaseRoleOptions().dbName("postgres")
+                        .creationStatements(creationStatements));
+        TestCase.assertEquals(204, databaseResponse.getRestResponse().getStatus());
+
+        DatabaseResponse credsResponse = vault.database().creds("new-role");
+        TestCase.assertEquals(200, credsResponse.getRestResponse().getStatus());
+
+        TestCase.assertTrue(credsResponse.getCredential().getUsername().contains("new-role"));
+        String prefix = Arrays.stream(credsResponse.getLeaseId().split("([^\\/]+)$"))
+                .map(str -> str.substring(0, str.length() - 1)).findFirst().get();
+
+        final VaultResponse response = vault.leases().revokeForce(prefix);
         assertEquals(204, response.getRestResponse().getStatus());
     }
-
 }
